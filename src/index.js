5cef32a8cd74ebb3bf8d89319b96232d8d2e0448
Paweesuda-Thippayanasa <paweesuda.thippayanasa@gmail.com>
2025-01-14 18:55:27 +0700

    remove console log in index.js

--- src/index.js
+++ src/index.js
@@ -15,12 +15,6 @@ const port = process.env.SERVER_PORT || 8001;
 
 let conn = null;
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
-console.log('DB_PORT:', process.env.DB_PORT);
-
 
 const initMysql = async () => {
 	conn = await mysql.createConnection({
